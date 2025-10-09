a612bd551c5037eb6b88523be14b363aebe33251
Cavad93 <cavad.93@yandex.ru>
2025-10-09 08:56:36 +0300

    Update manager.py

--- GGG 2/calib/manager.py
+++ GGG 2/calib/manager.py
@@ -1,6 +1,7 @@
 import os, time
 import numpy as np
 from .selector import CalibratorSelector, nll, brier
+from .holdout_manager import HoldoutCalibManager  # НОВОЕ
 
 class OnlineCalibManager:
     def __init__(self):
@@ -8,9 +9,9 @@ class OnlineCalibManager:
         self.roll_cal = None
         self.roll_buf = []
         self.roll_y = []
-        self.selector = CalibratorSelector()
-        self.roll_hours = int(os.getenv("CALIB_ROLLING_H","48"))
-        self.min_n = int(os.getenv("CALIB_MIN_N","300"))
+        self.holdout_mgr = HoldoutCalibManager()  # НОВОЕ
+        self.roll_hours = int(os.getenv("CALIB_ROLLING_H","72"))  # ИЗМЕНЕНО: было 48
+        self.min_n = int(os.getenv("CALIB_MIN_N","200"))  # ИЗМЕНЕНО: было 300
 
     def update(self, p_raw, y, ts):
         self.roll_buf.append((ts, float(p_raw)))
@@ -23,7 +24,12 @@ class OnlineCalibManager:
         if len(self.roll_y) >= self.min_n:
             pr = np.array([p for _,p in self.roll_buf])
             yr = np.array([y for _,y in self.roll_y])
-            _, self.roll_cal, _ = self.selector.fit_pick(pr, yr)
+            
+            # ИЗМЕНЕНО: используем hold-out валидацию
+            cal, holdout_nll = self.holdout_mgr.fit_with_holdout(pr, yr)
+            if cal is not None:
+                self.roll_cal = cal
+                print(f"[calib] Retrained with hold-out NLL={holdout_nll:.4f}")
 
     def fit_global(self, p_raw_hist, y_hist):
         _, self.global_cal, _ = self.selector.fit_pick(p_raw_hist, y_hist)
