6395b4dc7ca5eade4a5ac7cc761f4d13866dc027
Cavad93 <cavad.93@yandex.ru>
2025-10-21 00:32:25 +0300

    Delete GGG 2 directory

--- GGG 2/calib/init.py
+++
@@ -1,5 +0,0 @@
-from .platt import PlattCalibrator, TemperatureScaler
-from .beta import BetaCalibrator
-from .isotonic import IsotonicCalibrator
-from .bbq import BBQCalibrator
-from .selector import CalibratorSelector
