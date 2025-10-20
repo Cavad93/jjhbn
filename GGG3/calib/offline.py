2d391eb47dccfa7ce543481edc4a863ba57671e9
Cavad93 <cavad.93@yandex.ru>
2025-10-21 02:18:03 +0300

    Delete GGG3/calib directory

--- GGG3/calib/offline.py
+++
@@ -1,4 +0,0 @@
-def fit_forward_split(cal, ts, p_pred, y, ts_train_end, ts_test_start):
-    mask_calib = (ts >= ts_train_end) & (ts < ts_test_start)
-    cal.fit(p_pred[mask_calib], y[mask_calib])
-    return cal
